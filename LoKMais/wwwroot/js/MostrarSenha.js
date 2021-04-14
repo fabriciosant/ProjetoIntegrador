5286d66774549348227b1279855b8d4933b05f9c
Fabricio <fabriciosan47@gmail.com>
2021-04-14 17:18:47 -0400

    [REF] Layout Criar Usuario
    alterar o layout de criar usuario, inserio a propriedade de NomeCompleto , e criar arquivo separado para exibir senha.

---
+++ LoKMais/wwwroot/js/MostrarSenha.js
@@ -0,0 +1,15 @@
+﻿function mostrarOcutarSenha() {
+    var senha = document.getElementById("senha")
+    if (senha.type == "password") {
+        senha.type = "text"
+    } else {
+        senha.type = "password"
+    }
+
+    var confirmasenha = document.getElementById("confirmasenha")
+    if (confirmasenha.type == "password") {
+        confirmasenha.type = "text"
+    } else {
+        confirmasenha.type = "password"
+    }
+}

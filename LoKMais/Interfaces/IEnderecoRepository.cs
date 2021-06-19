094b3dbd726d40f597189ac7a0dd83c0c2d98039
Fabricio <fabriciosan47@gmail.com>
2021-06-19 13:58:31 -0400

    [Tarefa] Detalhes de endereço

---
+++ LoKMais/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,14 @@
+﻿using LoKMais.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoKMais.Interfaces
+{
+    public interface IEnderecoRepository : IResositoryBase<Endereco>
+    {
+        Task<IList<Endereco>> BuscarTodosAsync();
+        Task<Endereco> BuscarEnderecoPorIdAsync(Guid enderecoId);
+    }
+}
---
+++ LoKMais/Repositories/EnderecoRepository.cs
@@ -0,0 +1,28 @@
+﻿using LoKMais.Data;
+using LoKMais.Interfaces;
+using LoKMais.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoKMais.Repositories
+{
+    public class EnderecoRepository : RepositoryBase<Endereco>, IEnderecoRepository
+    {
+        public EnderecoRepository(LkContextDB contexto): base(contexto) { }
+
+        public async Task<IList<Endereco>> BuscarTodosAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<Endereco> BuscarEnderecoPorIdAsync(Guid enderecoId)
+        {
+            var result = await _dbSet.FirstOrDefaultAsync(endereco => endereco.EnderecoId == enderecoId);
+           
+            return result;
+        }
+    }
+}
